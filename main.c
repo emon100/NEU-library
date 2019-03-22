f46be2e2b3bf2c45286fcd4c5968d98dcbf9e247
emon100 <emon100@qq.com>
2019-03-23 03:09:09 +0800

    Fix menu

--- main.c
+++ main.c
@@ -16,119 +16,76 @@ int main(void) {
     book_recovery(book_data);
     //菜单部分开始
     while(1){
-    system("cls");
-    printf("欢迎使用图书管理系统, 请输入您的账号以继续:\n");
-    scanf("%d", &user_id);
-    fflush(stdin);
-    validity = login(user_id,person_data);
-    switch (validity) {
-        case no_user        :
-            printf("请重新登录!\t错误提示:没有该用户\n\n输入任何内容以继续:\n");
-            fflush(stdin);
-            getchar();
-            fflush(stdin);
-            break;
-        case password_err   :
-            printf("请重新登录!\t错误提示:密码错误!\n\n输入任何内容以继续:\n");
-            fflush(stdin);
-            getchar();
-            fflush(stdin);
-            break;
-        case as_reader      :
-            system("cls");
-            printf("\t\t\t\t尊敬的读者,感谢您使用图书管理系统,请输入指令来选择功能\n"
-                   "\t\t\t\t[1]总览信息与查询\n"
-                   "\t\t\t\t[2]读者中心\n"
-                   "\t\t\t\t[3]更换用户\n"
-                   "\t\t\t\t[4]帮助\n"
-                   "\t\t\t\t[5]退出\n");
-            fflush(stdin);//吃回车
-            while (choice = getchar()) {
-                fflush(stdin);//吃回车
-                switch (choice) {
-                    case '1'    :
-                        system("cls");
-                        information_reader(book_data);
-                        break;
-                    case '2'    :
-                        system("cls");
-                        reader_center(book_data, current_user);
-                        break;
-                    case '3'    :
-                        system("cls");
-                        exit_flag=1;
-                        break;
-                    case '4'    :
-                        system("cls");
-                        help();
-                        break;
-                    case '5'    :
-                        system("cls");
-                        printf("Thank you for using, goodbye!\n");
-                        return 0;
-                    default     :
-                        system("cls");
-                        printf("输入错误请重新输入!\n");
-                        break;
-                }
-                if(exit_flag==1){
-                    exit_flag=0;
-                    break;
-                }
-                fflush(stdin);//吃回车
+        system("cls");
+        printf("\t\t\t欢迎使用图书管理系统, 请输入您的账号以继续:\n");
+        scanf("%d", &user_id);
+        fflush(stdin);
+        validity = login(user_id,person_data);
+        switch (validity) {
+            case no_user        :
+                printf("请重新登录!\t错误提示:没有该用户\n\n输入任何内容以继续:\n");
+                fflush(stdin);
+                getchar();
+                fflush(stdin);
+                break;
+            case password_err   :
+                printf("请重新登录!\t错误提示:密码错误!\n\n输入任何内容以继续:\n");
+                fflush(stdin);
+                getchar();
+                fflush(stdin);
+                break;
+            case as_reader      :
+                system("cls");
                 printf("\t\t\t\t尊敬的读者,感谢您使用图书管理系统,请输入指令来选择功能\n"
                        "\t\t\t\t[1]总览信息与查询\n"
-                   "\t\t\t\t[2]读者中心\n"
-                   "\t\t\t\t[3]更换用户\n"
-                   "\t\t\t\t[4]帮助\n"
-                   "\t\t\t\t[5]退出\n");
-            }
-            break;
-        case as_admin     :
-            system("cls");
-            printf("\t\t\t\t尊敬的管理员,感谢您使用图书管理系统,请输入指令来选择功能\n"
-                   "\t\t\t\t[1]总览信息与查询\n"
-                   "\t\t\t\t[2]读者中心\n"
-                   "\t\t\t\t[3]管理系统\n"
-                   "\t\t\t\t[4]更换用户\n"
-                   "\t\t\t\t[5]帮助\n"
-                   "\t\t\t\t[6]退出\n");
-            fflush(stdin);
-            while (choice = getchar()) {
+                       "\t\t\t\t[2]读者中心\n"
+                       "\t\t\t\t[3]更换用户\n"
+                       "\t\t\t\t[4]帮助\n"
+                       "\t\t\t\t[5]退出\n");
                 fflush(stdin);//吃回车
-                switch (choice) {
-                    case '1'    :
-                        system("cls");
-                        information_admin(book_data, person_data);
+                while (choice = getchar()) {
+                    fflush(stdin);//吃回车
+                    switch (choice) {
+                        case '1'    :
+                            system("cls");
+                            information_reader(book_data);
+                            break;
+                        case '2'    :
+                            system("cls");
+                            reader_center(book_data, current_user);
+                            break;
+                        case '3'    :
+                            system("cls");
+                            exit_flag=1;
+                            break;
+                        case '4'    :
+                            system("cls");
+                            help();
+                            break;
+                        case '5'    :
+                            system("cls");
+                            printf("Thank you for using, goodbye!\n");
+                            return 0;
+                        default     :
+                            system("cls");
+                            printf("输入错误请重新输入!\n");
+                            break;
+                    }
+                    if(exit_flag==1){
+                        exit_flag=0;
                         break;
-                    case '2'    :
-                        system("cls");
-                        reader_center(book_data, current_user);
-                        break;
-                    case '3'    :
-                        system("cls");
-                        manage(book_data, person_data);
-                        break;
-                    case '4'    :
-                        system("cls");
-                        exit_flag=1;
-                        break;
-                    case '5'    :
-                        system("cls");
-                        help();
-                        break;
-                    case '6'    :
-                        system("cls");
-                        printf("感谢您的使用,下次再见!\n");return 0;
-                    default     :
-                        system("cls");
-                        printf("请再次输入指令!\n");
-                        break;
+                    }
+                    fflush(stdin);//吃回车
+                    printf("\t\t\t\t尊敬的读者,感谢您使用图书管理系统,请输入指令来选择功能\n"
+                           "\t\t\t\t[1]总览信息与查询\n"
+                       "\t\t\t\t[2]读者中心\n"
+                       "\t\t\t\t[3]更换用户\n"
+                       "\t\t\t\t[4]帮助\n"
+                       "\t\t\t\t[5]退出\n");
                 }
-                if(exit_flag==1){
-                    exit_flag=0;
-                    break;
-                }
+                break;
+            case as_admin     :
+                system("cls");
                 printf("\t\t\t\t尊敬的管理员,感谢您使用图书管理系统,请输入指令来选择功能\n"
                        "\t\t\t\t[1]总览信息与查询\n"
                        "\t\t\t\t[2]读者中心\n"
@@ -136,8 +93,51 @@ int main(void) {
                        "\t\t\t\t[4]更换用户\n"
                        "\t\t\t\t[5]帮助\n"
                        "\t\t\t\t[6]退出\n");
-                fflush(stdin);//吃回车
-            }
+                fflush(stdin);
+                while (choice = getchar()) {
+                    fflush(stdin);//吃回车
+                    switch (choice) {
+                        case '1'    :
+                            system("cls");
+                            information_admin(book_data, person_data);
+                            break;
+                        case '2'    :
+                            system("cls");
+                            reader_center(book_data, current_user);
+                            break;
+                        case '3'    :
+                            system("cls");
+                            manage(book_data, person_data);
+                            break;
+                        case '4'    :
+                            system("cls");
+                            exit_flag=1;
+                            break;
+                        case '5'    :
+                            system("cls");
+                            help();
+                            break;
+                        case '6'    :
+                            system("cls");
+                            printf("感谢您的使用,下次再见!\n");return 0;
+                        default     :
+                            system("cls");
+                            printf("请再次输入指令!\n");
+                            break;
+                    }
+                    if(exit_flag==1){
+                        exit_flag=0;
+                        break;
+                    }
+                    printf("\t\t\t\t尊敬的管理员,感谢您使用图书管理系统,请输入指令来选择功能\n"
+                           "\t\t\t\t[1]总览信息与查询\n"
+                           "\t\t\t\t[2]读者中心\n"
+                           "\t\t\t\t[3]管理系统\n"
+                           "\t\t\t\t[4]更换用户\n"
+                           "\t\t\t\t[5]帮助\n"
+                           "\t\t\t\t[6]退出\n");
+                    fflush(stdin);//吃回车
+                }
             break;
         }
     }
